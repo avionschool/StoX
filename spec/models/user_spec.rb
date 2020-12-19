9c03e06fec25491c48459ee271856d470789e56f
Ligaya Reyes <l.reyes062018@gmail.com>
2020-12-19 10:27:56 +0800

    added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) { User.new }
+
+  context "Validations" do
+
+    it "1. is not valid without name" do
+      user.save
+      expect(user).to_not be_valid
+      expect(user.errors).to be_present
+      expect(user.errors.to_h.keys).to include(:name)
+    end
+
+    it "2. is not valid without email" do
+      user.save
+      expect(user).to_not be_valid
+      expect(user.errors).to be_present
+      expect(user.errors.to_h.keys).to include(:email)
+    end
+
+    it "3. is not valid without role_id" do
+      user.save
+      expect(user).to_not be_valid
+      expect(user.errors).to be_present
+      expect(user.errors.to_h.keys).to include(:role_id)
+    end
+
+    it "4. is valid with attributes" do
+      user.name = "Sample"
+      user.email = "sample@sample.com"
+      user.role_id = "4"
+      user.save
+      expect(user).to be_valid
+      expect(user).to be_truthy
+    end
+
+  end
 end
