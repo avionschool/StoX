7d265ef7f93910cd32ae56e78df2a8489af78033
Ligaya Reyes <l.reyes062018@gmail.com>
2021-01-12 21:16:46 +0800

    added price to transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_121901) do
+ActiveRecord::Schema.define(version: 2021_01_12_131435) do
 
   create_table "buyer_stocks", force: :cascade do |t|
     t.integer "quantity"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_121901) do
     t.integer "stock_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "price"
     t.index ["stock_id"], name: "index_transactions_on_stock_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
